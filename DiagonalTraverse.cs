669bbdb39383da852dab2f65b4f5aa5b927a6c0a
Helga222 <barykina.o@yandex.ru>
2021-08-28 22:50:17 +0300

    Edited code

--- DiagonalTraverse.cs
+++ DiagonalTraverse.cs
@@ -1,42 +1,44 @@
-
-https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1167/
-public class Solution {
-    public int[] FindDiagonalOrder(int[][] mat) {
-        
-
-        int n=mat[0].Length;//rows
-        int m=mat.Length;//cols
-        int [] arr= new int [m*n];
-        int k=0;
-        int z=mat[0].Length;
-
-
-        int r=0;
-        int c=0;
-        for (int d=0;d<n+m-1;d++){
+//https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1167/
+public class Solution
+{
+    public int[] FindDiagonalOrder(int[][] mat)
+    {
+        int rows = mat.Length; //rows
+        int cols = mat[0].Length; //cols
+        int[] arr = new int[cols * rows];
+        int index = 0;
+        int row = 0;
+        int column = 0;
+        for (int d = 0; d < rows + cols - 1; d++)
+        {
             ArrayList list = new ArrayList();
-            if (d<m) { r=0;}
-            else { r=d-m+1;}
-            
-            if(d<m) { c=d;}
-            else { c=m-1;}
-        
-        while (r<n && c>-1) {
-            list.Add(mat[r][c]);
-            r++;
-            c--;
-        }    
-        if (d % 2 == 0) {
-            list.Reverse();
-        } 
+            if (d < cols)
+            {
+                row = 0;
+                column = d;
+            }
+            else
+            {
+                row = d - cols + 1;
+                column = cols - 1;
+            }
 
-        for (int i=0;i<list.Count;i++){
-            arr[k]=(int)list[i];
-            k++;
-        }    
+            while (row < rows && column > -1)
+            {
+                list.Add(mat[row][column]);
+                row++;
+                column--;
+            }
+            if (d % 2 == 0)
+            {
+                list.Reverse();
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                arr[index] = (int) list[i];
+                index++;
+            }
         }
-       // arr[0]=n;
         return arr;
-        
     }
 }
